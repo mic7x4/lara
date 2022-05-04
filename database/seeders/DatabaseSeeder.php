4ce432e19d9b16dfb6e308f20c55de00767a6b5a
mic7x4 <mic7x4@gmail.com>
2022-05-04 21:30:01 +0200

    adding some authentication

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,10 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
+use App\Models\User;
 use App\Models\Listing;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +16,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(4)->create();
+        // \App\Models\User::factory(4)->create();
 
-        Listing::factory(6)->create();
+        $user = User::factory()->create([
+            'name'=>'John Doe',
+            'email' => 'johndoe@gmail.com'
+        ]);
+
+        Listing::factory(6)->create([
+            'user_id' =>  $user->id
+        ]);
         // Listing::create(
         //     [
         //         'title' => 'Laravel Senior Developer', 
